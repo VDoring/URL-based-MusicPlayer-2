da273f2176c0d0b672ca5667b90d27b741e74256
Doring <d0211468@kowon.dongseo.ac.kr>
2021-06-21 15:51:51 +0900

    사용가능한 링크 추출 및 링크 실행 구현
    사용가능한 링크가 있는 Mlist.txt파일의 라인에서 메모 등의 다른 요소를 제거하고 링크만 가져올 수 있는 기능을 구현하였습니다.
    그리고 그 링크를 바탕으로 os 라이브러리를 이용해 해당 컴퓨터의 명령프롬프트를 사용함으로써 크롬 시크릿모드로 링크가 실행되게 하였습니다.

--- MP2 CLI/play_all_1_TopBottom.py
+++ MP2 CLI/play_all_1_TopBottom.py
@@ -5,6 +5,7 @@ import time
 #전체플레이 모드-위에서 아래순서로 곡을 실행합니다.
 #리턴값:없음
 def playTopBottom():
+    # Mlist.txt 파일 존재 확인 #
     is_file_available = os.path.isfile('Mlist.txt') # Mlist.txt 파일이 올바른 위치에 존재하는지 확인
     if is_file_available == False: # Mlist.txt 파일이 없다면
         print('[!] Mlist.txt 파일을 찾을 수 없습니다 [!]')
@@ -12,15 +13,23 @@ def playTopBottom():
         time.sleep(1.5)
         return
 
+    # 유효한 링크 수 파악 #
     mlist = open('Mlist.txt','r', encoding='utf-8') # 파일 열기
     mlist_all_lines_index = mlist.readlines() # 파일의 모든 글자들을 리스트 형식으로 저장
     mlist_lines_count = len(mlist_all_lines_index) # 파일 속 총 라인 개수
-    mlist_not_available_lines_count = 0 # 파일 속 링크가 아닌 라인 개수
+    mlist_available_links_locate = [] # 파일 속 링크인 라인의 위치
     for i in range(len(mlist_all_lines_index)): # 파일의 글자 라인 수 만큼 반복하면서 링크가 아닌 것을 파악하고, 총 링크갯수 카운트에서 제외
-        if ('http' not in mlist_all_lines_index[i]) or ('https' not in mlist_all_lines_index[i]): # 링크가 아닌 경우
-            mlist_not_available_lines_count += 1
-    mlist_available_lines_count = mlist_lines_count - mlist_not_available_lines_count # 파일 속 사용가능한 링크가 있는 라인 수
+        if 'http' in mlist_all_lines_index[i]: # 링크가 아닌 경우
+            mlist_available_links_locate.append(i)
+
+    # 링크 실행 #
+    mlist.seek(0) # 파일 라인을 처음 위치로 이동
+    for i in mlist_available_links_locate: # 사용가능한 링크 수만큼 반복
+        line_index = mlist_all_lines_index[i]
+        link = line_index.split()
+        os.system('start chrome --incognito ' + link[0]) # 링크가 포함된 크롬 시크릿모드 실행 명령어 실행
+        
 
     #디버그용
-    print(mlist_available_lines_count)
-    time.sleep(100)
+    #print(mlist_available_links_count)
+    #time.sleep(100)
